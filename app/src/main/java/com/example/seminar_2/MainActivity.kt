c59e959cbfce9bfb52ec0132c7d25a0be39e6f13
amsdevelops <amsdevelops@gmail.com>
2023-06-14 15:14:13 +0300

    Template added

--- app/src/main/java/com/example/seminar_2/MainActivity.kt
+++ app/src/main/java/com/example/seminar_2/MainActivity.kt
@@ -6,9 +6,12 @@ import com.example.seminar_2.databinding.ActivityMainBinding
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var isAccelerate = true
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,10 +27,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun createSpeedometerValues() = Observable.create<Int> {
-        var isAccelerate = true
         var speed = 0
         while (true) {
-            Thread.sleep(100)
+            Thread.sleep(Random.nextLong(500))
             if (isAccelerate) {
                 speed++
                 if (speed == 100) isAccelerate = false
