654c67e9632af9116572ddb8e588560f3b9149ec
amsdevelops <amsdevelops@gmail.com>
2023-06-14 15:06:30 +0300

    Template added

--- app/src/main/java/com/example/seminar_2/MainActivity.kt
+++ app/src/main/java/com/example/seminar_2/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.seminar_2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.seminar_2.databinding.ActivityMainBinding
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            createSpeedometerValues()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    binding.speedometer.text = it.toString()
+                }
+        }
     }
+
+    fun createSpeedometerValues() = Observable.create<Int> {
+        var isAccelerate = true
+        var speed = 0
+        while (true) {
+            Thread.sleep(100)
+            if (isAccelerate) {
+                speed++
+                if (speed == 100) isAccelerate = false
+            } else {
+                speed--
+                if (speed == 0) isAccelerate = true
+            }
+            it.onNext(speed)
+        }
+    }.subscribeOn(Schedulers.io())
 }
